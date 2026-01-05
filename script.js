85f4494fe20210f5924675f591f33970fcb535f2
Arifendryw999 <133835563+Alvaromeo999@users.noreply.github.com>
2026-01-05 12:58:11 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("orderForm").addEventListener("submit",function(e){
 e.preventDefault();
 const n=nama.value,p=produk.value,j=jumlah.value,h=harga.value;
 const t=h*j;
-const pesan=`PESANAN DAPUR BUNDA%0ANama:${n}%0AProduk:${p}%0AJumlah:${j}%0ATotal:Rp ${t}`;
-document.getElementById("nota").innerHTML=`<b>Total Bayar:</b> Rp ${t}`;
+document.getElementById("nota").innerHTML=`<b>Total Bayar:</b> Rp ${t.toLocaleString('id-ID')}`;
 document.querySelector(".nota-actions").style.display="block";
+const pesan=`PESANAN DAPUR BUNDA%0ANama:${n}%0AProduk:${p}%0AJumlah:${j}%0ATotal:Rp ${t}`;
 window.open(`https://wa.me/6285229455301?text=${pesan}`,"_blank");
 });
 function printNota(){window.print();}
